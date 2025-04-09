5d022078f724a5c7a7a6e15870279f3eec75acde
Chandrababu-nagilli <92324757+Chandrababu-nagilli@users.noreply.github.com>
2025-04-09 23:15:46 +0530

    Update BaseImageImporter.py

--- imgfac/BaseImageImporter.py
+++ imgfac/BaseImageImporter.py
@@ -3,6 +3,7 @@ import sys
 import time
 import json
 import logging
+import platform
 from .FactoryUtils import launch_inspect_and_mount, qemu_convert_cmd, subprocess_check_output
 from .ApplicationConfiguration import ApplicationConfiguration
 from .PersistentImageManager import PersistentImageManager
@@ -23,7 +24,6 @@ class BaseImageImporter(object):
         """
         Import file as a base_image and return the resulting BaseImage object
         """
-
         g = launch_inspect_and_mount(self.image_file, readonly=True)
         inspection = g.inspect_os()
 
@@ -34,10 +34,9 @@ class BaseImageImporter(object):
         i_distro = g.inspect_get_distro(os_root)
         i_major_version = g.inspect_get_major_version(os_root)
         i_minor_version = g.inspect_get_minor_version(os_root)
-        i_arch = g.inspect_get_arch(os_root)
 
-        ins_res = "guestfs inspection result - type: %s - name: %s - distro: %s - major version: %s - minor version: %s - arch: %s" % \
-                  (i_type, i_name, i_distro, i_major_version, i_minor_version, i_arch)
+        ins_res = "guestfs inspection result - type: %s - name: %s - distro: %s - major version: %s - minor version: %s" % \
+                  (i_type, i_name, i_distro, i_major_version, i_minor_version)
         self.log.debug(ins_res)
 
         if i_type != "linux":
@@ -58,11 +57,8 @@ class BaseImageImporter(object):
         else:
             raise Exception("Unsupported distro for import: %s" % (i_distro))
 
-        # Set the correct architecture for RHEL 9 support on s390x
-        if i_distro == "rhel" and i_major_version == 9 and i_arch == "s390x":
-            tdl_arch = "s390x"
-        else:
-            tdl_arch = "x86_64"
+        # Determine system architecture (e.g., x86_64, s390x)
+        tdl_arch = platform.machine()
 
         ftime = time.strftime("%Y-%m-%d--%H:%M:%S", time.localtime())
         tname = "%s-%s-import-%s" % (tdl_os_name, tdl_os_version, ftime)
@@ -86,12 +82,14 @@ class BaseImageImporter(object):
         pim.add_image(base_image)
         base_image.template = tdl_template
 
-        # Convert input image to qcow2 format
-        self.log.debug("Converting and saving input file %s to final data location %s" % \
+        # Convert the input image to qcow2
+        # The input image can be in any format that libguestfs understands
+        # Here we convert it to qcow2 - If it is already in qcow2 this is benign
+        # and in some cases can tidy up and serialize it
+        self.log.debug("Converting and saving input file %s to final data location %s" %
                        (self.image_file, base_image.data))
         cmd = qemu_convert_cmd(self.image_file, base_image.data)
         (stdout, stderr, retcode) = subprocess_check_output(cmd)
-
         base_image.status = "COMPLETE"
         base_image.percent_complete = 100
         pim.save_image(base_image)

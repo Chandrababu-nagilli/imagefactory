b78b1ee8ba7fc736b723ccdcaf3d355f5fe0ef8d
Chandrababu-nagilli <92324757+Chandrababu-nagilli@users.noreply.github.com>
2025-04-14 20:53:17 +0530

    Update BaseImageImporter.py

--- imgfac/BaseImageImporter.py
+++ imgfac/BaseImageImporter.py
@@ -9,7 +9,6 @@ from .PersistentImageManager import PersistentImageManager
 from .BaseImage import BaseImage
 from oz.ozutil import copyfile_sparse
 
-
 class BaseImageImporter(object):
 
     def __init__(self, image_file=None):
@@ -29,20 +28,26 @@ class BaseImageImporter(object):
 
         os_root = inspection[0]
 
-        i_type=g.inspect_get_type(os_root)
-        i_name=g.inspect_get_product_name(os_root)
-        i_distro=g.inspect_get_distro(os_root)
-        i_major_version=g.inspect_get_major_version(os_root)
-        i_minor_version=g.inspect_get_minor_version(os_root)
+        # Get architecture from inspection
+        i_arch = g.inspect_get_arch(os_root)  # New line to detect architecture
+        i_type = g.inspect_get_type(os_root)
+        i_name = g.inspect_get_product_name(os_root)
+        i_distro = g.inspect_get_distro(os_root)
+        i_major_version = g.inspect_get_major_version(os_root)
+        i_minor_version = g.inspect_get_minor_version(os_root)
 
-        ins_res = "guestfs inspection result - type: %s - name: %s - distro: %s - major version: %s - minor version: %s" % \
-                  (i_type, i_name, i_distro, i_major_version, i_minor_version)
+        ins_res = "guestfs inspection result - arch: %s - type: %s - name: %s - distro: %s - major version: %s - minor version: %s" % \
+                  (i_arch, i_type, i_name, i_distro, i_major_version, i_minor_version)
         self.log.debug(ins_res)
 
         if i_type != "linux":
             raise Exception("Can only import Linux distros into Factory at the moment")
 
-        if i_distro in [ 'centos', 'rhel', 'scientificlinux' ]:
+        # Add architecture validation
+        if i_arch not in ['x86_64', 's390x']:
+            raise Exception(f"Unsupported architecture: {i_arch}")
+
+        if i_distro in ['centos', 'rhel', 'scientificlinux']:
             tdl_os_name = "RHEL-%d" % (i_major_version)
             tdl_os_version = "%d" % (i_minor_version)
         elif i_distro == 'fedora':
@@ -60,32 +65,29 @@ class BaseImageImporter(object):
         ftime = time.strftime("%Y-%m-%d--%H:%M:%S", time.localtime())
         tname = "%s-%s-import-%s" % (tdl_os_name, tdl_os_version, ftime)
 
-        tdl_template="""<template>
+        tdl_template = """<template>
           <name>%s</name>
           <os>
             <name>%s</name>
             <version>%s</version>
-            <arch>x86_64</arch>
+            <arch>%s</arch>  <!-- Dynamic architecture insertion -->
             <install type='url'>
               <url>http://foo.com/imported/image/do/not/use/url</url>
             </install>
           </os>
           <description>image imported on %s</description>
         </template>
-        """ % (tname, tdl_os_name, tdl_os_version, ftime)
+        """ % (tname, tdl_os_name, tdl_os_version, i_arch, ftime)  # Added i_arch
 
         pim = PersistentImageManager.default_manager()
         base_image = BaseImage()
         pim.add_image(base_image)
-        base_image.template=tdl_template
-        # The input image can be in any format that libguestfs understands
-        # Here we convert it to qcow2 - If it is already in qcow2 this is benign
-        # and in some cases can tidy up and serialize it
-        self.log.debug("Converting and saving intput file %s to final data location %s" % \
+        base_image.template = tdl_template
+        self.log.debug("Converting and saving input file %s to final data location %s" % \
                        (self.image_file, base_image.data))
         cmd = qemu_convert_cmd(self.image_file, base_image.data)
         (stdout, stderr, retcode) = subprocess_check_output(cmd)
-        base_image.status="COMPLETE"
-        base_image.percent_complete=100
+        base_image.status = "COMPLETE"
+        base_image.percent_complete = 100
         pim.save_image(base_image)
         return base_image
